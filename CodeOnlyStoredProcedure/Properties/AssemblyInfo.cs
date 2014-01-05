4b85da8d10aa074edb6ef176dc8d0540b65cc23f
Abraham Heidebrecht <abraham@heidebrechtsoftwerks.com>
2014-01-04 23:55:04 -0500

    Bumped version numbers.

--- CodeOnlyStoredProcedure/Properties/AssemblyInfo.cs
+++ CodeOnlyStoredProcedure/Properties/AssemblyInfo.cs
@@ -2,38 +2,15 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("CodeOnlyStoredProcedure")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Code Only Stored Proecedures")]
+[assembly: AssemblyDescription("A library for easily calling Stored Procedures in .NET. Works great with Entity Framework Code First models. Code Only Stored Procedures will not create any Stored Procedures on your database. Instead, its aim is to make it easy to call your existing stored procedures by writing simple code.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Abraham Heidebrecht")]
 [assembly: AssemblyProduct("CodeOnlyStoredProcedure")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+[assembly: AssemblyCopyright("Copyright © Abraham Heidebrecht 2013 - 2014")]
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3b540587-06b3-4564-9372-cde3a3755fe1")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.6.0")]
-[assembly: AssemblyFileVersion("0.0.6.0")]
+[assembly: AssemblyVersion("0.0.7.0")]
+[assembly: AssemblyFileVersion("0.0.7.0")]
 
 #if NET40
 [assembly: InternalsVisibleTo("CodeOnlyTests-NET40")]
