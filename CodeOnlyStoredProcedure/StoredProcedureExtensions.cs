ffd5529b667921769d1377008e881235a4635ebe
abe545 <abe545@gmail.com>
2014-05-04 16:04:30 -0400

    Changed execution logic to build expression trees instead of using straight reflection. This fully implements #19

--- CodeOnlyStoredProcedure/StoredProcedureExtensions.cs
+++ CodeOnlyStoredProcedure/StoredProcedureExtensions.cs
@@ -11,6 +11,7 @@ using System.Linq;
 using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Linq.Expressions;
 
 namespace CodeOnlyStoredProcedure
 {
@@ -378,7 +379,7 @@ namespace CodeOnlyStoredProcedure
                 }
                 else
                 {
-                    var row   = (IRow)Activator.CreateInstance(typeof(Row<>).MakeGenericType(currentType));
+                    var row   = (IRowFactory)Activator.CreateInstance(typeof(RowFactory<>).MakeGenericType(currentType));
                     var names = Enumerable.Range(0, reader.FieldCount)
                                           .Select(i => reader.GetName(i))
                                           .ToArray();
@@ -387,7 +388,7 @@ namespace CodeOnlyStoredProcedure
                     {
                         token .ThrowIfCancellationRequested();
                         reader.GetValues(values);
-                        output.Add(row.Create(names, values, transformers));
+                        output.Add(row.CreateRow(names, values, transformers));
                     }
 
                     if (output.Count > 0)
@@ -638,67 +639,112 @@ namespace CodeOnlyStoredProcedure
             throw new NotSupportedException("Unable to determine the SqlDbType for the property. You can specify it by using a StoredProcedureParameterAttribute. Or prevent it from being mapped with the NotMappedAttribute.");
         }
 
-        private interface IRow
+        private interface IRowFactory
         {
             IEnumerable<string> UnfoundPropertyNames { get; }
-            object Create(string[] fieldNames, object[] values, IEnumerable<IDataTransformer> transformers);
+            object CreateRow(string[] fieldNames, object[] values, IEnumerable<IDataTransformer> transformers);
         }
 
-        private class Row<T> : IRow
+        private class RowFactory<T> : IRowFactory
             where T : new()
         {
-            private static   IDictionary<string, PropertyInfo>           props;
-            private static   IDictionary<string, IEnumerable<Attribute>> propertyAttributes;
-            private readonly HashSet<string>                             unfoundProps;
+            private static   IDictionary<string, Action<T, object, IEnumerable<IDataTransformer>>> setters = new Dictionary<string, Action<T, object, IEnumerable<IDataTransformer>>>();
+            private readonly HashSet<string>                                                       unfoundProps;
 
             public IEnumerable<string> UnfoundPropertyNames { get { return unfoundProps; } }
 
-            static Row()
+            static RowFactory()
             {
-                props = typeof(T).GetMappedPropertiesBySqlName();
-                propertyAttributes = props.ToDictionary(kv => kv.Key,
-                                                        kv => kv.Value
-                                                                .GetCustomAttributes(false)
-                                                                .Cast<Attribute>()
-                                                                .ToArray()
-                                                                .AsEnumerable());
+                var tType         = typeof(T);
+                var listType      = typeof(IEnumerable<IDataTransformer>);
+                var row           = Expression.Parameter(tType,          "row");
+                var val           = Expression.Parameter(typeof(object), "value");
+                var gts           = Expression.Parameter(listType,       "globalTransformers");
+                var xf            = typeof(DataTransformerAttributeBase).GetMethod("Transform");
+                var props         = tType.GetMappedPropertiesBySqlName();
+                var propertyAttrs = props.ToDictionary(kv => kv.Key,
+                                                       kv => kv.Value
+                                                               .GetCustomAttributes(false)
+                                                               .Cast<Attribute>()
+                                                               .ToArray()
+                                                               .AsEnumerable());
+                
+                foreach (var kv in props)
+                {
+                    var expressions = new List<Expression>();
+                    var iterType    = typeof(IEnumerator<IDataTransformer>);
+                    var iter        = Expression.Variable(iterType, "iter");
+
+                    IEnumerable<Attribute> attrs;
+                    if (propertyAttrs.TryGetValue(kv.Key, out attrs))
+                    {
+                        var propTransformers = attrs.OfType<DataTransformerAttributeBase>().OrderBy(a => a.Order);
+                        var propType         = Expression.Constant(kv.Value.PropertyType, typeof(Type));                        
+                        var xformType        = typeof(IDataTransformer);
+                        var attrsExpr        = Expression.Constant(attrs, typeof(IEnumerable<Attribute>));
+                        var transformer      = Expression.Variable(xformType, "transformer");
+                        var endFor           = Expression.Label("endForEach");
+
+                        var doTransform = Expression.IfThen(
+                            Expression.Call(transformer, xformType.GetMethod("CanTransform"), val, propType, attrsExpr),
+                            Expression.Assign(val,
+                                Expression.Call(transformer, xformType.GetMethod("Transform"), val, propType, attrsExpr)));
+
+                        expressions.Add(Expression.Assign(iter, Expression.Call(gts, listType.GetMethod("GetEnumerator"))));
+                        var loopBody = Expression.Block(
+                            new[] { transformer },
+                            Expression.Assign(transformer, Expression.Property(iter, iterType.GetProperty("Current"))),
+                            doTransform);
+
+                        expressions.Add(Expression.Loop(
+                            Expression.IfThenElse(Expression.Call(iter, typeof(IEnumerator).GetMethod("MoveNext")),
+                                                  loopBody,
+                                                  Expression.Break(endFor)),
+                            endFor));
+
+
+                        foreach (var xform in propTransformers)
+                            expressions.Add(Expression.Assign(val, Expression.Call(Expression.Constant(xform), xf, val, propType)));
+                    }
+
+                    Expression conv;
+                    if (kv.Value.PropertyType.IsValueType)
+                        conv = Expression.Unbox(val, kv.Value.PropertyType);
+                    else
+                        conv = Expression.Convert(val, kv.Value.PropertyType);
+
+                    expressions.Add(Expression.Assign(Expression.Property(row, kv.Value), conv));
+
+                    var body   = Expression.Block(new[] { iter }, expressions.ToArray());
+                    var lambda = Expression.Lambda<Action<T, object, IEnumerable<IDataTransformer>>>(body, row, val, gts);
+
+                    setters.Add(kv.Key, lambda.Compile());
+                }
             }
 
-            public Row()
+            public RowFactory()
             {
-                unfoundProps = new HashSet<string>(props.Keys);
+                unfoundProps = new HashSet<string>(setters.Keys);
             }
 
-            public object Create(
+            public object CreateRow(
                 string[]                      fieldNames, 
                 object[]                      values,
                 IEnumerable<IDataTransformer> transformers)
             {
                 var row = new T();
-                PropertyInfo pi;
+                Action<T, object, IEnumerable<IDataTransformer>> set;
 
                 for (int i = 0; i < values.Length; i++)
                 {
                     var name = fieldNames[i];
-                    if (props.TryGetValue(name, out pi))
+                    if (setters.TryGetValue(name, out set))
                     {
                         var value = values[i];
                         if (DBNull.Value.Equals(value))
                             value = null;
 
-                        var attrs = propertyAttributes[name];
-                        foreach (var xform in transformers)
-                        {
-                            if (xform.CanTransform(value, pi.PropertyType, attrs))
-                                value = xform.Transform(value, pi.PropertyType, attrs);
-                        }
-
-                        var propTransformers = attrs.OfType<DataTransformerAttributeBase>()
-                                                    .OrderBy(a => a.Order);
-                        foreach (var xform in propTransformers)
-                            value = xform.Transform(value, pi.PropertyType);
-
-                        pi.SetValue(row, value, null);
+                        set(row, value, transformers);
                         unfoundProps.Remove(name);
                     }
                 }
