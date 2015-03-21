f5e9455155ced44ad8ad57d2360cbc4d609a0cda
abe545 <abe545@gmail.com>
2015-03-20 23:34:25 -0400

    Removed unused code & tests

--- CodeOnlyStoredProcedure/StoredProcedureParameters/TableValuedParameter.cs
+++ CodeOnlyStoredProcedure/StoredProcedureParameters/TableValuedParameter.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using Microsoft.SqlServer.Server;
 
 namespace CodeOnlyStoredProcedure
 {
@@ -32,7 +36,7 @@ namespace CodeOnlyStoredProcedure
             parm.ParameterName = ParameterName;
             parm.SqlDbType     = SqlDbType.Structured;
             parm.TypeName      = TypeName;
-            parm.Value         = values.ToTableValuedParameter(valueType);
+            parm.Value         = CrateValuedParameter(values, valueType);
 
             return parm;
         }
@@ -49,6 +53,45 @@ namespace CodeOnlyStoredProcedure
                 ++i;
 
             return i;
+        }
+
+        private static IEnumerable<SqlDataRecord> CrateValuedParameter(IEnumerable table, Type enumeratedType)
+        {
+            Contract.Requires(table != null);
+            Contract.Requires(enumeratedType != null);
+            Contract.Ensures(Contract.Result<IEnumerable<SqlDataRecord>>() != null);
+
+            var recordList = new List<SqlDataRecord>();
+            var columnList = new List<SqlMetaData>();
+            var props      = enumeratedType.GetMappedProperties().ToList();
+
+            foreach (var pi in props)
+            {
+                var attr = pi.GetCustomAttributes(false)
+                             .OfType<StoredProcedureParameterAttribute>()
+                             .FirstOrDefault();
+
+                if (attr != null)
+                    columnList.Add(attr.CreateSqlMetaData(pi.Name, pi.PropertyType));
+                else
+                    columnList.Add(pi.PropertyType.CreateSqlMetaData(pi.Name, null, null, null, null));
+            }
+
+            // copy the input list into a list of SqlDataRecords
+            foreach (var row in table)
+            {
+                var record = new SqlDataRecord(columnList.ToArray());
+                for (int i = 0; i < columnList.Count; i++)
+                {
+                    // locate the value of the matching property
+                    var value = props[i].GetValue(row, null);
+                    record.SetValue(i, value);
+                }
+
+                recordList.Add(record);
+            }
+
+            return recordList;
         }
     }
 }
